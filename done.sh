b5805bbe43a44a95add5f57b677fcc593133f95d
thanhren <40245250+thanhren@users.noreply.github.com>
2024-09-29 19:51:44 +0700

    Initial commit

---
+++ done.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Append the content of Task contract/erc20tx.txt to erc20.txt
+cat "Task contract/contracterc20tx.txt" >> "Task contract/contracterc20.txt"
+
+# Append the content of Task contract/erc721.txt to erc20.txt
+cat "Task contract/contracterc721.txt" >> "Task contract/contracterc20.txt"
+
+# Append the content of Task contract/erc20tx.txt to erc20.txt
+cat "Task contract/contracterc721tx.txt" >> "Task contract/contracterc20.txt"
+
+# Append the content of Task contract/erc721.txt to erc20.txt
+cat "Task contract/contractperc20.txt" >> "Task contract/contracterc20.txt"
+
+# Append the content of Task contract/erc20tx.txt to erc20.txt
+cat "Task contract/contractperc20tx.txt" >> "Task contract/contracterc20.txt"
+
+# Append the content of Task contract/erc721.txt to erc20.txt
+cat "Task contract/contractprivate.txt" >> "Task contract/contracterc20.txt"
+
+# Append the content of Task contract/erc20tx.txt to erc20.txt
+cat "Task contract/contractproxy.txt" >> "Task contract/contracterc20.txt"
+
+# Append the content of Task contract/erc721.txt to erc20.txt
+cat "Task contract/contractproxytx.txt" >> "Task contract/contracterc20.txt"
+
+# Append the content of Task contract/erc20tx.txt to erc20.txt
+cat "Task contract/contracthardhat.txt" >> "Task contract/contracterc20.txt"
+
+
+# Append the content of Task contract/erc20tx.txt to erc20.txt
+cat "Task contract/contracterc20.txt" >> "Task contract/full.txt"
+# Append the content of Task contract/erc20tx.txt to erc20.txt
+echo "" >> "Task contract/full.txt"
+
+echo "Done!!!"
